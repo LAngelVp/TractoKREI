bbeefcd438f38eb73cd090ec352e48e88736b5dd
Luis Angel <neal.ocaffry@gmail.com>
2025-12-12 11:50:09 -0600

    Creacion de ruta principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeroHome } from './components/hero-home/hero-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HeroHome
+    }
+];
